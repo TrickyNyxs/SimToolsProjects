90c6ea31475e5546a4010ccb92dec308e672f336
Dante <dante.tengblad@gmail.com>
2026-01-28 13:09:08 +0100

    SimulationTime

--- Tasks.py
+++ Tasks.py
@@ -32,33 +32,4 @@ eP_Solver = asol.CVode(eP_Problem)
 eP_Solver.reset() # Why is this needed here?
 t_sol, x_sol = eP_Solver.simulate(4, 1000) # simulate(tf, ncp)
 
-# Plot Results
-plt.figure()
-
-plt.plot(t_sol, x_sol[:,0], label=r'$x(t)$')
-plt.plot(t_sol, x_sol[:,1], label=r'$y(t)$')
-plt.plot(t_sol, x_sol[:,2], label=r'$v_x(t)$')
-plt.plot(t_sol, x_sol[:,3], label=r'$v_y(t)$')
-
-plt.xlabel('t')
-plt.ylabel('state value')
-plt.legend()
-plt.title(eP_Problem.name)
-plt.grid(True)
-plt.show()
-
-# Our Solution with BDF4
-exp_sim = BDF4Solver.BDF4(eP_Problem) #Create a BDF solver
-exp_sim.reset()
-t, y = exp_sim.simulate(4)
-exp_sim.plot()
-mpl.show()
-
-
-# Provided BDF2 Solution
-exp_sim3 = BDF2Solver.BDF_2(eP_Problem) #Create a BDF solver
-exp_sim3.reset()
-t, y = exp_sim3.simulate(4)
-exp_sim3.plot()
-mpl.show()
 
